de9a304cd91a38fbd0e25d1bbe11d5a82f827174
James Bremner <james@ravenspoint.com>
2021-04-29 10:53:35 -0400

    Split graph into forward, backward and turning links

--- src/unirobot.cpp
+++ src/unirobot.cpp
@@ -5,6 +5,13 @@
 #include <vector>
 #include <algorithm>
 
+struct sBiLink
+{
+    int src;
+    int dst;
+    int dir;        // allowed orientations
+};
+
 std::vector< std::string > ParseSpaceDelimited(
     const std::string& l )
 {
@@ -27,13 +34,17 @@ std::vector< std::string > ParseSpaceDelimited(
     return token;
 }
 
-void read( const std::string& fname )
+std::vector< sBiLink > read( 
+    const std::string& fname,
+    std::vector< int>& vTurn )
 {
     std::ifstream inf( fname );
     if( ! inf.is_open() )
     {
         std::cout << "cannot open " << fname << "\n";
     }
+    std::vector< sBiLink > vBiLink;
+    vTurn.clear();
     std::string line;
     int src, dst, dir;
     while( std::getline(inf,line))
@@ -50,9 +61,11 @@ void read( const std::string& fname )
                 std::cout << "bad link: " << line << "\n";
                 exit(1);
             }
-            src = atoi( token[1].c_str() );
-            dst = atoi( token[2].c_str() );
-            dir = atoi( token[3].c_str() );
+            sBiLink l;
+            l.src = atoi( token[1].c_str() );
+            l.dst = atoi( token[2].c_str() );
+            l.dir = atoi( token[3].c_str() );
+            vBiLink.push_back( l );
             break;
 
             case 's':
@@ -80,7 +93,7 @@ void read( const std::string& fname )
                 std::cout << "bad turning node: " << line << "\n";
                 exit(1);
             }    
-            src = atoi( token[1].c_str() );
+            vTurn.push_back( atoi( token[1].c_str() ) );
             break;
 
             default:
@@ -88,16 +101,79 @@ void read( const std::string& fname )
             break;
         }
     }
+    std::cout << vBiLink.size() << " bidirectional links input\n";;
+    return vBiLink;
+}
+
+void split( 
+    std::vector< sBiLink >& vBiLink,
+    std::vector< int >& vTurn )
+{
+    std::vector< sBiLink > vForward;
+    std::vector< sBiLink > vBack;
+
+    for( auto& l : vBiLink )
+    {
+        switch( l.dir )
+        {
+            case 0:
+            vForward.push_back( l );
+            vBack.push_back( l );
+            break;
+
+            case 1:
+            vForward.push_back( l );
+            break;
+
+            case 2:
+            vBack.push_back( l );
+            break;   
+        }
+    }
+
+    std::cout << "\nForward links\n";
+    for( auto& l : vForward )
+    {
+        std:: cout
+            << std::to_string( l.src ) 
+            << "f - "
+            << std::to_string( l.dst ) 
+            << "f\n";
+    }
+    std::cout << "\nBack links\n";
+    for( auto& l : vBack )
+    {
+        std:: cout
+            << std::to_string( l.src ) 
+            << "b - "
+            << std::to_string( l.dst ) 
+            << "b\n";
+    }
+    std::cout << "\nTurning Links\n";
+    for( auto& n : vTurn )
+    {
+        std:: cout
+            << std::to_string( n ) 
+            << "f - "
+            << std::to_string( n ) 
+            << "b\n";
+    }
 }
 main( int argc, char* argv[] )
 {
     std::cout << "unirobot\n";
 
+    // check command line
     if( argc != 2 )
     {
         std::cout << "usage: unirobot <inputfilename>\n";
         exit(0);
     }
 
-    read(argv[1]);
+    // read input file
+    std::vector< int> vTurn;
+    auto vBiLink = read( argv[1], vTurn );
+
+    // split graph
+    split( vBiLink, vTurn );
 }
